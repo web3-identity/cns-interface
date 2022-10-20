0309f9bb0aaf1cf5dcc9e5004c6c2f0216f0f254
Angelia <iangeliaq@gmail.com>
2022-10-20 17:06:19 +0800

    fix: svg

--- src/components/Spin/index.tsx
+++ src/components/Spin/index.tsx
@@ -8,14 +8,14 @@ const Spin: React.FC<HTMLAttributes<HTMLDivElement>> = ({ className }) => {
       <svg className="spin" width="240" height="240" viewBox="0 0 240 240" color="currentColor">
         <defs>
           <linearGradient id="linear-gradient1">
-            <stop offset="0%" stop-color="currentColor" />
-            <stop offset="50%" stop-color="currentColor" stop-opacity="50%" />
+            <stop offset="0%" stopColor="currentColor" />
+            <stop offset="50%" stopColor="currentColor" stopOpacity="50%" />
           </linearGradient>
           <linearGradient id="linear-gradient2">
-            <stop offset="50%" stop-color="currentColor" stop-opacity="50%" />
-            <stop offset="100%" stop-color="currentColor" stop-opacity="0%" />
+            <stop offset="50%" stopColor="currentColor" stopOpacity="50%" />
+            <stop offset="100%" stopColor="currentColor" stopOpacity="0%" />
           </linearGradient>
-          <circle id="semi-circle" cx="120" cy="120" r="100" stroke-width="20" stroke-dasharray="314 1000" fill="none" />
+          <circle id="semi-circle" cx="120" cy="120" r="100" strokeWidth="20" strokeDasharray="314 1000" fill="none" />
         </defs>
         <g>
           <use href="#semi-circle" stroke="url('#linear-gradient1')" />
