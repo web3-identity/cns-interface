6fc8e5e25d8cda238df11d752185176423c8a52a
Angelia <iangeliaq@gmail.com>
2022-10-25 17:53:34 +0800

    fix: avatar address

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,10 +1,11 @@
 import React, { type HTMLAttributes, useEffect, useRef } from 'react';
 import jazzIcon from '@utils/jazzIcon';
-import { addressToNumber } from '@utils/addressUtils';
+import { addressToNumber, convertCfxToHex } from '@utils/addressUtils';
 import removeAllChild from '@utils/removeAllChild';
 
 const Avatar: React.FC<HTMLAttributes<HTMLDivElement> & { address: string | null | undefined; diameter: number }> = ({ address, diameter }) => {
-  const renderAddress = addressToNumber(address);
+  const renderAddress = addressToNumber(convertCfxToHex(address!));
+  console.log(address, renderAddress)
   const avatarContainerRef = useRef<HTMLDivElement>(null!);
   useEffect(() => {
     const avatarDom = jazzIcon(diameter, renderAddress);
