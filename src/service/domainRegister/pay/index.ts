496281564c4574bc33286a0ee9246cbaa6d4e76b
SSSensational <37439786+SSSensational@users.noreply.github.com>
2022-11-01 17:54:41 +0800

    web2 pay

--- src/service/domainRegister/pay/index.ts
+++ src/service/domainRegister/pay/index.ts
@@ -1,5 +1,6 @@
 export * from './price';
 export * from './web3';
+export * from './web2/pc';
 import { atomFamily, useRecoilValue } from 'recoil';
 import { setRecoil } from 'recoil-nexus';
 import { persistAtomWithDefault } from '@utils/recoilUtils';
--- src/service/domainRegister/pay/web2/pc/index.ts
+++ src/service/domainRegister/pay/web2/pc/index.ts
@@ -2,21 +2,43 @@ import { selectorFamily, useRecoilValue, useRecoilRefresher_UNSTABLE } from 'rec
 import { fetchApi } from '@utils/fetch';
 import { commitInfoState, getCommitInfo } from '@service/domainRegister/commit';
 
-export const postOrder = (commitmentHash: string, params: object) => fetchApi({ path: `registers/order/${commitmentHash}`, method: 'POST', params });
-export const getOrder = (commitmentHash: string) => fetchApi({ path: `registers/order/${commitmentHash}`, method: 'GET' });
+const postOrder = (commitmentHash: string, domain: string) =>
+  fetchApi({
+    path: `registers/order/${commitmentHash}`,
+    method: 'POST',
+    params: {
+      trade_provider: 'wechat',
+      trade_type: 'native',
+      description: domain,
+    },
+  });
+
+const getOrder = (commitmentHash: string) => fetchApi({ path: `registers/order/${commitmentHash}`, method: 'GET' });
+export const isOrderPaid = (commitmentHash: string) =>
+  fetchApi<Response>({ path: `registers/order/${commitmentHash}`, method: 'GET' }).then((res) => {
+    return !!res?.trade_state && res.trade_state === 'SUCCESS' ? true : null;
+  });
+
 export const refreshRegisterOrder = (domain: string) => {
   const commitInfo = getCommitInfo(domain);
   if (!commitInfo) return;
-  fetchApi({ path: `registers/order/refresh-url/${commitInfo.commitmentHash}`, method: 'PUT' });
+  fetchApi({
+    path: `registers/order/refresh-url/${commitInfo.commitmentHash}`,
+    method: 'PUT',
+    params: {
+      trade_provider: 'wechat',
+      trade_type: 'native',
+      description: domain,
+    },
+  });
 };
 
 interface Response {
   code_url: string;
   commit_hash: string;
-  trade_no: string;
-  trade_provider: string;
-  trade_type: string;
+  trade_state: string;
 }
+
 const makeOrderQuery = selectorFamily<Response, string>({
   key: 'makeOrder',
   get:
@@ -30,11 +52,7 @@ const makeOrderQuery = selectorFamily<Response, string>({
 
         if (getRes?.code === 50000) {
           if (getRes?.message === 'record not found') {
-            const postRes: any = await postOrder(commitInfo.commitmentHash, {
-              trade_provider: 'wechat',
-              trade_type: 'native',
-              description: domain,
-            });
+            const postRes: any = await postOrder(commitInfo.commitmentHash, domain);
 
             if (postRes?.code === 50000) {
               throw new Error(getRes.message);
