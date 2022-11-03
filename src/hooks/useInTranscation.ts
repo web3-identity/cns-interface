b2c12085b06f6d6818646b174bd9895fb80f95fc
SSSensational <37439786+SSSensational@users.noreply.github.com>
2022-11-03 15:26:07 +0800

    fix: inTranscation not stop when transcation error

--- src/hooks/useInTranscation.ts
+++ src/hooks/useInTranscation.ts
@@ -3,10 +3,14 @@ import { useState, useCallback } from 'react';
 const useInTranscation = <T extends (params: any) => Promise<any> | null | undefined>(transcationFunc: T) => {
   const [inTranscation, setInTranscation] = useState(false);
   const execTranscation = useCallback(async (params: any) => {
-    setInTranscation(true);
-    const res = await transcationFunc(params);
-    setInTranscation(false);
-    return res;
+    try {
+      setInTranscation(true);
+      const res = await transcationFunc(params);
+      setInTranscation(false);
+      return res;
+    } catch(_) {
+      setInTranscation(false);
+    }
   }, [transcationFunc]) as T;
 
   return { inTranscation, execTranscation };
