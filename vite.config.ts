056078058166d03e8568f07fa1aeb21d44f8d099
SSSensational <sss18201672034@163.com>
2022-12-13 17:11:30 +0800

    upgrade vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import compression  from 'vite-plugin-compression'
 import Unocss from 'unocss/vite';
 import presetIcons from '@unocss/preset-icons';
 import presetWind from '@unocss/preset-wind';
@@ -63,6 +64,7 @@ export default defineConfig({
       },
     }),
     react(),
+    compression(),
   ],
   resolve: {
     alias: {
@@ -85,6 +87,13 @@ export default defineConfig({
     minify: false,
     rollupOptions: {
       plugins: [visualizer()],
+      output: {
+        manualChunks: id => {
+          if(id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
     },
   },
   optimizeDeps: {
